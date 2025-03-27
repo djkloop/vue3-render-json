d13906a7d941d1c0cfec88a5dac99a8e0270010f
djkloop <1044032256@qq.com>
2025-03-27 18:33:39 +0800

    init

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginBabel } from '@rsbuild/plugin-babel'
 import { pluginVue } from '@rsbuild/plugin-vue'
 import { pluginVueJsx } from '@rsbuild/plugin-vue-jsx'
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin'
 import { UnoCSSRspackPlugin } from '@unocss/webpack/rspack'
-import CompressionPlugin from 'compression-webpack-plugin'
+// import CompressionPlugin from 'compression-webpack-plugin'
+
+const isOpenRsDoctor = import.meta.env.PUBLIC_RSDOCTOR === 'true'
 
 export default defineConfig({
   plugins: [
@@ -14,11 +17,12 @@ export default defineConfig({
     pluginVueJsx(),
   ],
   tools: {
-    rspack: {
-      plugins: [
+    rspack(_, { appendPlugins }) {
+      appendPlugins([
+        // 如果有这个插件 RsdoctorRspackPlugin 没效果
         UnoCSSRspackPlugin(),
-        new CompressionPlugin(),
-      ],
+        isOpenRsDoctor && new RsdoctorRspackPlugin(),
+      ])
     },
   },
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,19 @@
 /// <reference types="@rsbuild/core/types" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+  import type { DefineComponent } from 'vue'
 
   // biome-ignore lint/complexity/noBannedTypes: reason
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare const PUBLIC_RSDOCTOR: string
+
+declare interface ImportMetaEnv {
+  readonly PUBLIC_RSDOCTOR: string
+}
+
+declare interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
